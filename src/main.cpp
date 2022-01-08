eca6c9347413b5e25844317088b0fb3882a1a430
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-01-09 06:07:08 +1300

    I put all of the motors,sensors,etc into a header

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,9 @@
 
 #include "main.h"
 
-// Stating Components
-	// V5 Parts
-		// Controller
-			// Name of the controller is master since it's the main controller incase we use 2 controllers
-			Controller master (E_CONTROLLER_MASTER);
-			// This is if we use a 2nd controller
-			Controller partner (E_CONTROLLER_PARTNER);
-		// Motors
-			Motor LBM(3, E_MOTOR_GEARSET_18 , false);
-			Motor LFM(2, E_MOTOR_GEARSET_18, false);
-			Motor RFM(10, E_MOTOR_GEARSET_18, true);
-			Motor RBM(1, E_MOTOR_GEARSET_18, true);
-			Motor DR4BL(18, E_MOTOR_GEARSET_36, false);
-			Motor DR4BR(19, E_MOTOR_GEARSET_36, true);
-		// Distance
-			Distance LIFTO(20);
-	// Tri port
-		// Piston
-			ADIDigitalOut CLAMPY(7);
-		// Encoders
-			ADIEncoder LYEN(1, 2, false);
-			ADIEncoder RYEN(3, 4, false);
+// This is all the electronics
+#include "asignment.h"
+
 // Global Variables
 	bool usercontrol = false;
 	bool autonGo = false;
